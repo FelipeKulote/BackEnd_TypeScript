35220b549df4a4097d006f6df60bf4cc210a4f1d
Felipe Augusto <lipe_augustobq@hotmail.com>
2022-11-24 21:12:15 -0300

    feat: criação do método update

--- src/users/service/user.service.ts
+++ src/users/service/user.service.ts
@@ -1,6 +1,7 @@
 import { IUserEntity } from '../entities/user.entity';
 import { UserDto } from './dto/user.dto';
 import { randomUUID } from 'node:crypto';
+import { PartialUserDto } from './dto/partialUser.dto';
 
 export class UserService {
   private users: IUserEntity[] = [];
@@ -10,5 +11,14 @@ export class UserService {
     return userEntity;
   }
 
-  async updateUser()
+  async updateUser(userData: PartialUserDto): Promise<IUserEntity> {
+    this.users.map((user, index) => {
+      if (user.id === userData.id) {
+        const modifiedUser = Object.assign(user, userData);
+        this.users.splice(index, 1, modifiedUser);
+      }
+    });
+    const updatedUser = this.users.find((user) => user.id === userData.id);
+    return updatedUser;
+ }   
 }

93834169af24be3613ef030a956e693f3d90dcfd
Felipe Augusto <lipe_augustobq@hotmail.com>
2022-11-28 16:49:34 -0300

    feat: create no controller

--- src/users/user.controller.ts
+++ src/users/user.controller.ts
@@ -1,6 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
-import { get } from 'http';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { IUserEntity } from './entities/user.entity';
+import { UserDto } from './service/dto/user.dto';
 import { UserService } from './service/user.service';
 
 @Controller()
@@ -10,5 +10,22 @@ export class UserController {
   @Get()
   async getAllUsers(): Promise<IUserEntity[]> {
     return await this.service.getAllUsers();
+  }
+
+  @Post()
+  async createUser(
+    @Body() { name, email, password, cpf, role }: UserDto,
+  ): Promise<IUserEntity> {
+    try {
+      return await this.service.createUser({
+        name,
+        email,
+        password,
+        cpf,
+        role,
+      });
+    } catch (err) {
+      console.log(err);
+    }
   }
 }

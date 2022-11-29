df91f545096e119e3aad27b3d3851a1dfe55e477
Felipe Augusto <lipe_augustobq@hotmail.com>
2022-11-29 09:04:59 -0300

    feat: criação e implementação do métdo getUserById e deleteUserById

--- src/users/service/user.service.ts
+++ src/users/service/user.service.ts
@@ -26,17 +26,23 @@ export class UserService {
     return this.users;
   }
 
-    async deleteUserById(userId: string): Promise<boolean> {
-      const deletedUser = this.users.find((user) => user.id === userId);
-      if (!deletedUser) {
-        return false;
-      } 
-      this.users.map((user, index) => {
-        if (user.id === userId) {
-          this.users.splice(index, 1);
-        }
-      });
-      return true;
+  async deleteUserById(userId: string): Promise<boolean> {
+    const deletedUser = this.users.find((user) => user.id === userId);
+    if (!deletedUser) {
+      return false;
     }
-  
+    this.users.map((user, index) => {
+      if (user.id === userId) {
+        this.users.splice(index, 1);
+      }
+    });
+    return true;
+  }
+
+  async getUserById(userId: string): Promise<IUserEntity> {
+    const findUser = this.users.find((user) => user.id === userId);
+    if (!findUser) {
+      
+    }
+  }
 }
--- src/users/user.controller.ts
+++ src/users/user.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Get, Patch, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { IUserEntity } from './entities/user.entity';
 import { PartialUserDto } from './service/dto/partialUser.dto';
 import { UserDto } from './service/dto/user.dto';
@@ -11,6 +19,14 @@ export class UserController {
   @Get()
   async getAllUsers(): Promise<IUserEntity[]> {
     return await this.service.getAllUsers();
+  }
+  @Get(':id')
+  async getUserById(@Param('id') userId: string): Promise<IUserEntity> {
+    try {
+      return await this.service.getUserById(userId);
+    } catch (err) {
+      console.log('Usuário não encontrado');
+    }
   }
 
   @Post()
@@ -34,6 +50,20 @@ export class UserController {
   async updateUser(@Body() userData: PartialUserDto): Promise<IUserEntity> {
     try {
       return await this.service.updateUser(userData);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  @Delete(':id')
+  async deleteUserById(@Param('id') userId: string): Promise<string> {
+    try {
+      const deletedUser = await this.service.deleteUserById(userId);
+      if (deletedUser) {
+        return 'Usuário deletado com sucesso';
+      } else {
+        return 'Usuário não encontrado';
+      }
     } catch (err) {
       console.log(err);
     }

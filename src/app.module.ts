2fd09e5eb1fc2744f77ba009e64398c549c2ff3f
Felipe Augusto <lipe_augustobq@hotmail.com>
2022-11-29 18:41:32 -0300

    feat: Criação do repository

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { DatabaseModule } from './prisma/database.module';
 import { UserService } from './users/service/user.service';
 import { UserController } from './users/user.controller';
 
 @Module({
+  imports: [DatabaseModule],
   controllers: [UserController],
   providers: [UserService],
 })
---
+++ src/users/userRepository.ts
@@ -0,0 +1,35 @@
+import { PrismaService } from 'src/prisma/prisma.service';
+import { IUserEntity } from './entities/user.entity';
+import { PartialUserDto } from './service/dto/partialUser.dto';
+
+export class UserRepository {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async createUser(user: IUserEntity): Promise<IUserEntity> {
+    const CreatedUser = await this.prisma.user.create({ data: user });
+    return CreatedUser;
+  }
+  async updateUser(user: PartialUserDto): Promise<IUserEntity> {
+    const UpdatedUser = await this.prisma.user.update({
+      where: { id: user.id },
+      data: user,
+    });
+    return UpdatedUser;
+  }
+  async deleteUser(id: string): Promise<IUserEntity> {
+    const DeletedUser = await this.prisma.user.delete({
+      where: { id: id },
+    });
+    return DeletedUser;
+  }
+  async findAllUser(): Promise<IUserEntity[]> {
+    const AllUsers = await this.prisma.user.findMany();
+    return AllUsers;
+  }
+  async findUserById(id: string): Promise<IUserEntity> {
+    const FoundUser = await this.prisma.user.findUniqueOrThrow({
+      where: { id: id },
+    });
+    return FoundUser;
+  }
+}
